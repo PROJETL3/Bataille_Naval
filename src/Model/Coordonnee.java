3f5d1043c18c7e6e37276ff8de08e904c5a8747e
creAtiveuh <Jude@MacBook-Pro-de-Jude.local>
2014-11-28 15:52:15 +0100

    Modification
    coordonnee > refais
    delete > Hajar

--- src/Model/Coordonnee.java
+++ src/Model/Coordonnee.java
@@ -1,38 +1,51 @@
+package Model;
 
-	/*
-	* To change this license header, choose License Headers in Project Properties.
-	* To change this template file, choose Tools | Templates
-	* and open the template in the editor.
-	*/
-	/**
-	*
-	* @author Jude Modified By  Fatimaezzahra
-	*/
+/**
+ * @version 2.1
+ * @author Abdel, Fatimaezzahra, Hajar, Jude;
+ */
 public class Coordonnee {
-	public int x;
-	public int y;
-	
-	public Coordonnee (int x ,int y){
-		this.x=x;
-		this.y=y;
-		}
-	
-	
-	
+    
+    public int x;	// coordonnee x
+    public int y;	// coordonnee y
+    int etat; 		// -1 aucune action , 0 il y a un bateau , 1 coord toucher
+    
+    public Coordonnee(int X,int Y,int T)
+    {
+        this.x=X;
+        this.y=Y;
+        this.etat=T;
+    }
+    
+    public int getX()
+    {
+    	return this.x ;
+    }
 
-	public int getX(){
-		return this.x ;
-	}	
-	public int getY(){
-		return this.y;
-	}	
-	public void SetX(int x){
-		this.x=x;
+    public int getY()
+    {
+    	return this.y;
+    }
+    
+    public int getEtat()
+    {
+    	return(this.etat);
+    }
+    
+    public void SetX(int x)
+    {
+    	this.x=x;
 	}
-	
-	public void SetY(int y){
+
+	public void SetY(int y)
+	{
 		this.y=y;
 	}
 	
+	public void SetEtat(int etat)
+	{
+		this.etat=etat;
 	}
+    
+}
 
