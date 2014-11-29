eb87d48fc1c0440005a65e3fe4c9de3687bd3e93
Abdel <abdelnaji91@gmail.com>
2014-11-29 12:32:03 +0100

    Abstraite sur qui permettra de codé Humain et IA
    Classe Joueur a terminé ...

--- src/Model/Joueur.java
+++ src/Model/Joueur.java
@@ -1,5 +1,31 @@
 package Model;
 
-public class Joueur {
+abstract class Joueur{
+
+	private cuirasse cui1;
+	private cuirasse cui2;
+	private zodiaque zod;
+	private sm sm;
+	private pa pa;
+
+
+
+
+
+	Joueur(String Nom)
+	{
+		System.out.println("création du joueur "+Nom) ;
+
+		cui1 = new cuirasse("c1");
+		cui2 = new cuirasse("c2");
+		zod = new zodiaque("zodiaque");
+		sm = new sm("sous-marin");
+		pa = new pa("porte avion");
+	}
+
+	public abstract int tir(int x, int y);  // coder selon le type de joueur (humain ou IA)
+
+
+
 
 }
