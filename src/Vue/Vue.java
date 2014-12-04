802c4708563ab0149d41ad6a17de94d3f2526e91
creAtiveuh <Jude@MacBook-Pro-de-Jude.local>
2014-12-04 10:29:32 +0100

    Update
    update vue

--- src/Vue/Vue.java
+++ src/Vue/Vue.java
@@ -1,5 +1,20 @@
 package Vue;
 
-public class Vue {
+import javax.swing.JFrame;
 
+public class Vue extends JFrame
+{
+	private static final long serialVersionUID = 1L;
+	public Vue(String P)
+	{
+
+		JFrame frame = new JFrame(P);			// crée  + nomme la fenetre
+		//JLabel label = new JLabel("IcI");			//on crée un label 
+		frame.setBounds(10, 43, 1350, 540);		//regle la position et la taille
+		//frame.getContentPane().add(label);			//on ajoute le label créé
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //fermeture du frame
+		frame.setVisible(true);
+		
+		
+	}
 }
